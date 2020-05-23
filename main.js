edda79e757ffc22a21e11004d447eb805fb66e5b
Justin Beynon <JMB335@gmail.com>
2020-05-23 12:39:28 -0500

    Updated CSS styling. Adding clear grid button that includes the ability for a user to set the size of the grid

--- main.js
+++ main.js
@@ -1,16 +1,40 @@
-for (i = 0; i < 100 ; i++) {
-  let gridContainer = document.getElementById('grid-container');
-  var square = document.createElement("div");
+const gridContainer = document.querySelector('#grid-container')
+
+function setSize(rowsCols){
+    gridContainer.style.setProperty('--grid-rows', rowsCols);
+    gridContainer.style.setProperty('--grid-cols', rowsCols);
   
-  square.setAttribute("id", `grid${i}`);
-  square.setAttribute("class", `gridSquare`);
+  let rowCount = rowsCols * rowsCols
   
-  gridContainer.appendChild(square); 
+  var child = gridContainer.lastElementChild;  
+  while (child) { 
+      gridContainer.removeChild(child); 
+      child = gridContainer.lastElementChild; 
+    } 
+
+    for (i = 0; i < rowCount ; i++) {
+      var square = document.createElement("div");
+      square.setAttribute("id", `grid${i}`);
+      square.setAttribute("class", `gridSquare`);
+      gridContainer.appendChild(square); 
+  }
+
+  [...document.querySelectorAll('.gridSquare')].forEach(function(item) {
+    item.addEventListener('mouseover', function() 
+      {item.style.background = "#FF7558"});
+     });
 }
 
-[...document.querySelectorAll('.gridSquare')].forEach(function(item) {
-  item.addEventListener('mouseover', function() {
-    if (item.style.background === "purple") {item.style.background = "orange";}
-    else {item.style.background = "purple"};
-  });
-   });
+//var sizePrompt1 = prompt("How many rows and columns should the Etchy Sketch have?");
+//setSize(sizePrompt1);
+
+setSize(10);
+
+let resetButton = document.querySelector('#clear-container')
+resetButton.addEventListener('click', function() {
+  [...document.querySelectorAll('.gridSquare')].forEach(function(item) {
+    item.style.background = "#848599"})
+  sizePrompt2 = prompt("Set a new size for the grid");
+  setSize(sizePrompt2);
+});
+
