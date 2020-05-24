9eaf0f443177526e56f8c7e4da872f30a230f427
Justin Beynon <JMB335@gmail.com>
2020-05-24 09:05:12 -0500

    Added color randomizer and comments to all files

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 const gridContainer = document.querySelector('#grid-container')
 
+//Creates a raondom color
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+//Dynamically modifies the CSS variable that controls the number of rows and columns in the ES
 function setSize(rowsCols){
-    gridContainer.style.setProperty('--grid-rows', rowsCols);
-    gridContainer.style.setProperty('--grid-cols', rowsCols);
+    gridContainer.style.setProperty('--grid-colsrows', rowsCols);
   
   let rowCount = rowsCols * rowsCols
   
+  //clears the grid when the function runs
   var child = gridContainer.lastElementChild;  
   while (child) { 
       gridContainer.removeChild(child); 
       child = gridContainer.lastElementChild; 
     } 
 
+  //appends X number of new divs to the container div, based on the user's input
     for (i = 0; i < rowCount ; i++) {
       var square = document.createElement("div");
       square.setAttribute("id", `grid${i}`);
@@ -19,22 +31,23 @@ function setSize(rowsCols){
       gridContainer.appendChild(square); 
   }
 
+  //adds hover event listener to each div. 
   [...document.querySelectorAll('.gridSquare')].forEach(function(item) {
     item.addEventListener('mouseover', function() 
-      {item.style.background = "#FF7558"});
+      {item.style.background = getRandomColor()});
      });
 }
 
-//var sizePrompt1 = prompt("How many rows and columns should the Etchy Sketch have?");
-//setSize(sizePrompt1);
+//Initial primpt when loading the screen
+var sizePrompt1 = prompt("How many rows and columns should the Etchy Sketch have?");
+setSize(sizePrompt1);
 
-setSize(10);
-
+//Reset button
 let resetButton = document.querySelector('#clear-container')
 resetButton.addEventListener('click', function() {
   [...document.querySelectorAll('.gridSquare')].forEach(function(item) {
     item.style.background = "#848599"})
-  sizePrompt2 = prompt("Set a new size for the grid");
+  sizePrompt2 = prompt("Set a new size for the grid", sizePrompt1);
   setSize(sizePrompt2);
 });
 
